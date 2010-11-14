079d22188aeba78e582fc5f3cb28db4363dd1ffb
Jakub Okoński <kuba@okonski.org>
2010-11-14 20:58:41 +0100

    Add exception handling and loading from secrets.yml.

--- api_browser.rb
+++ api_browser.rb
@@ -1,9 +1,20 @@
 #!/usr/bin/env ruby
 $LOAD_PATH << './app/models/'
 require 'active_record'
+require 'yaml'
 
 ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database => 'ruby.db'
 
 Dir.glob('app/models/*.rb').each do |model|
   require model.scan(/\/([a-z]+).rb/).flatten.first
+end
+
+begin
+  GitHub::User.delete_all
+  $user = GitHub::User.create(YAML::load_file('config/secrets.yml')["user"])
+  
+rescue ActiveRecord::StatementInvalid
+  puts 'Error: Did you run rake db:migrate?'
+  puts 'Trying to do it for you...'
+  system 'rake db:migrate'
 end
