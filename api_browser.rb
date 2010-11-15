69c3eb7ae1711adb0290654bd3ba985ace83ad56
Jakub Okoński <kuba@okonski.org>
2010-11-15 18:42:18 +0100

    User ruby symbols for properties inside YAML. Retry code after migration.

--- api_browser.rb
+++ api_browser.rb
@@ -13,12 +13,14 @@ end
 
 begin
   GitHub::User.delete_all
-  $user = GitHub::User.create(YAML::load_file('config/secrets.yml')["user"])
+  $user = GitHub::User.create(YAML::load_file('config/secrets.yml')[:user])
   
 rescue ActiveRecord::StatementInvalid
   puts 'Error: Did you run rake db:migrate?'
   puts 'Trying to do it for you...'
   system 'rake db:migrate'
+  retry
+  puts "Retrying after migration"
 end
 
 uri = URI.parse("http://github.com/api/v2/yaml/user/show/#{$user.login}")
