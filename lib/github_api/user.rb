2ca45f444c3d9093f69a0928ddd1f15a0a0838ab
Jakub Okoński <kuba@okonski.org>
2010-11-20 18:29:10 +0100

    Allow array of symbols to be passed for GitHub::User.get.

--- lib/github_api/user.rb
+++ lib/github_api/user.rb
@@ -3,7 +3,7 @@ module GitHub
     attr_accessor :login, :token
     
     def get(login)
-      if login == :self
+      if [:self, :me].include? login
         login = self.login
       end
       return GitHub::Browser.get "/user/show/#{login}"
