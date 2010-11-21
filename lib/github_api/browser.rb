87e5b82004dfd0bbaf0786c83705fd63b7b51024
Jakub Okoński <kuba@okonski.org>
2010-11-21 11:10:10 +0100

    Add simple url encoding to browser.

--- lib/github_api/browser.rb
+++ lib/github_api/browser.rb
@@ -10,12 +10,14 @@ module GitHub
     
     # Runs HTTP GET request at given uri
     def self.get(uri)
+      uri = uri.gsub(" ","+")
       puts "Requesting #{URI.parse(self.base_uri + uri)}"
       Net::HTTP.get URI.parse(self.base_uri + uri)
     end
     
     # Runs HTTP POST requests with options such as GitHub::User.auth_info
     def self.post(uri, options = {})
+      uri = uri.gsub(" ","+")
       puts "Requesting #{URI.parse(self.base_uri + uri)} with options: #{options}"
       Net::HTTP.post_form URI.parse(self.base_uri + uri), options
     end
