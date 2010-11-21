1c90941518846525695f4ffc4a8d11e59a100d03
Jakub Okoński <kuba@okonski.org>
2010-11-21 09:59:06 +0100

    Add diagnostic messages to Browser.

--- lib/github_api/browser.rb
+++ lib/github_api/browser.rb
@@ -1,4 +1,3 @@
-require 'singleton'
 module GitHub
   class Browser
     include Singleton
@@ -8,10 +7,12 @@ module GitHub
     end
     
     def self.get(uri)
+      puts "Requesting #{URI.parse(self.base_uri + uri)}"
       Net::HTTP.get URI.parse(self.base_uri + uri)
     end
     
     def self.post(uri, options = {})
+      puts "Requesting #{URI.parse(self.base_uri + uri)} with options: #{options}"
       Net::HTTP.post_form URI.parse(self.base_uri + uri), options
     end
   end
