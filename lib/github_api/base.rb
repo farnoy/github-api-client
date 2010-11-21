570eb81e2fc665fc68f5538a6c9cb7a4a243e844
Jakub Okoński <kuba@okonski.org>
2010-11-21 09:59:36 +0100

    Add to_ary function for Base class, define GitHub::Helper for building objects form retrieved yaml.

--- lib/github_api/base.rb
+++ lib/github_api/base.rb
@@ -2,12 +2,31 @@ module GitHub
   class Base
     def build(options = {})
       options.each_pair do |k, v|
-        self.send :"#{k.to_sym}=", v
+        self.send "#{k.to_sym}=", v
       end
+    end
+    
+    def to_ary
+      return self.attributes
     end
     
     def method_missing(method, *args, &block)
       puts "Missing #{method}"
+    end
+  end
+  
+  class Helper
+    include Singleton
+    
+    def self.build_from_yaml(yaml)
+      yaml = YAML::load yaml
+      object = case
+        when yaml.has_key?('user') then [GitHub::User, 'user']
+      end
+      object[0] = object.first.new
+      object.first.build yaml[object[1]]
+      
+      object.first
     end
   end
 end
