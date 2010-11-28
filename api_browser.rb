d5c5d18adbf735565325498a4f0bc81be4a993f9
Jakub Okoński <kuba@okonski.org>
2010-11-28 20:02:09 +0100

    described sample usage

--- api_browser.rb
+++ api_browser.rb
@@ -3,12 +3,24 @@ $LOAD_PATH << './lib/' << './lib/github_api/'
 
 require 'github_api'
 
-#GitHub::User.search(' ').each do |user|
-  #puts user.id + " " + user.login_safe + " " + user.name_safe + " " + user.followers_safe.to_s
-#end
-p $user.get_followers :self
-#p GitHub::User.get('defunkt')
-#puts $user.post(:me)
-#$user.set ['user', 'show', $user.login], 'values[email]' => 'test@api.com'
-#puts $user.followers('schacon')
-#p GitHub::User.get('schacon')
+GitHub::User.search('chacon').each do |user|
+  puts user.name
+end
+
+# You'll probably get banned on GitHub, so better don't perform this
+#GitHub::Base.sync
+
+# Prints list of followers logins for user with login kneath
+GitHub::User.new(:login => 'kneath').fetch(:self, :followers).followers.each do |follower|
+  puts follower.login
+end
+
+# Fetches followers and self information for user defined in config/secrets.yml
+p $user.fetch(:self, :followers)
+
+# Gets simple User information for defunkt
+p GitHub::User.get('defunkt')
+
+# EXPERIMENTAL
+# Sets email for user defined in config/secrets.yml to test@api.com
+$user.set ['user', 'show', $user.login], 'values[email]' => 'test@api.com'
