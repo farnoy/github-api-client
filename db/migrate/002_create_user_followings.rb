26b987c5ad2df5fba4128405821d2030c5789fa5
Jakub Okoński <kuba@okonski.org>
2010-12-03 17:09:00 +0100

    there will be two tables for user followings and repo

---
+++ db/migrate/002_create_user_followings.rb
@@ -0,0 +1,13 @@
+class CreateUserFollowings < ActiveRecord::Migration
+  def self.up
+    create_table :user_followings, :id => false, :force => true do |t|
+      t.references :follower
+      t.references :following
+      t.integer :resource
+    end
+  end
+  
+  def self.down
+    drop_table :user_followings
+  end
+end
