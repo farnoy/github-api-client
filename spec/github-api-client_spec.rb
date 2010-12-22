66d286101a26e601b986804d1d327174b9dea6aa
Jakub Okoński <kuba@okonski.org>
2010-12-22 17:49:30 +0100

    Some more features.

--- spec/github-api-client_spec.rb
+++ spec/github-api-client_spec.rb
@@ -1,19 +1,57 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "GitHub Api Client" do
+  
   context "configuration" do
+    
     it "should create database on first run" do
       File.stubs(:exists?).returns(false)
       ActiveRecord::Migrator.expects(:migrate).with(GitHub::Config::Path[:migrations], nil).once
       GitHub::Config.setup
     end
+    
+    it "should not migrate database every time" do
+      File.stubs(:exists?).returns(true)
+      ActiveRecord::Migrator.expects(:migrate).never
+      GitHub::Config.setup
+    end
+    
+    context "options" do
+      
+      context "with enabled verbose" do
+        
+        it "should print messages" do
+          GitHub::Config::Options.expects(:"[]").with(:verbose).returns(true)
+          $stdout.expects(:puts).once
+          GitHub::Browser.get("/users/show/farnoy")
+        end
+        
+      end
+      
+      context "with disabled verbose" do
+        
+        it "should not print messages" do
+          GitHub::Config::Options.expects(:"[]").with(:verbose).returns(false)
+          $stdout.expects(:puts).never
+          GitHub::Browser.get("/users/show/farnoy")
+        end
+        
+      end
+                
+    end
   end
   
   context "basics" do
-    it "should save objects to database"
+    
+    it "should save objects to database" do
+      u = GitHub::User.get("farnoy")
+      u.should == GitHub::User.find(u.id)
+    end
+    
   end
   
   context "version" do
+    
     it "should assign properly" do
       GitHub::Config::Version.should == File.read('VERSION')
     end
@@ -21,5 +59,6 @@ describe "GitHub Api Client" do
     it "should alias CAPITALIZED name for maniacs" do
       GitHub::Config::Version.should == GitHub::Config::VERSION
     end
+    
   end
 end
