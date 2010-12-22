a65715d1f57df9daf8bc7c04249f72e5bbead5a8
Jakub Okoński <kuba@okonski.org>
2010-12-22 17:32:54 +0100

    Mocha and few tests.

--- spec/github-api-client_spec.rb
+++ spec/github-api-client_spec.rb
@@ -1,7 +1,25 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "GithubApiClient" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "GitHub Api Client" do
+  context "configuration" do
+    it "should create database on first run" do
+      File.stubs(:exists?).returns(false)
+      ActiveRecord::Migrator.expects(:migrate).with(GitHub::Config::Path[:migrations], nil).once
+      GitHub::Config.setup
+    end
+  end
+  
+  context "basics" do
+    it "should save objects to database"
+  end
+  
+  context "version" do
+    it "should assign properly" do
+      GitHub::Config::Version.should == File.read('VERSION')
+    end
+    
+    it "should alias CAPITALIZED name for maniacs" do
+      GitHub::Config::Version.should == GitHub::Config::VERSION
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,4 +9,5 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
   config.mock_with :rspec
+  config.mock_with :mocha
 end
