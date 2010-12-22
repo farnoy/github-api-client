30ad9ba34414fd2bae94b3109d2702e216556b66
Jakub Okoński <kuba@okonski.org>
2010-12-22 19:37:01 +0100

    Cucumber repos.

--- features/github-api-client.feature
+++ features/github-api-client.feature
@@ -3,7 +3,11 @@ Feature: Fetching Objects
   I just need to fire up few commands
 
   Scenario: Fetching user information
-    Given I fetch user 'schacon'
-    And I set verbose option to 'true'
+    Given I fetch user "schacon"
     Then My local database should contain that record
-    And That record's 'name' should be 'Scott Chacon'
+    And That record's "name" should be "Scott Chacon"
+  
+  Scenario: Fetching repo information
+    Given I fetch repo "mojombo/jekyll"
+    Then My local database should contain that record
+    And That record's "login" of the "owner" should be "mojombo"
--- features/step_definitions/github-api-client_steps.rb
+++ features/step_definitions/github-api-client_steps.rb
@@ -4,18 +4,26 @@ Before do
   end
 end
 
-Given /^I fetch user '(.*)'$/ do |login|
+Given /^I fetch user "(.*)"$/ do |login|
   @record = GitHub::User.get(login)
+end
+
+Given /^I fetch repo "(.*)"$/ do |permalink|
+  @record = GitHub::Repo.get(permalink)
 end
 
 Then /^My local database should contain that record$/ do
   @record.class.find(@record.id).should == @record
 end
 
-Then /^That record's '(.*)' should be '(.*)'$/ do |sig, prop|
+Then /^That record's "([^"]*)" should be "([^"]*)"$/ do |sig, prop|
   @record.send(sig.to_sym).should == prop
 end
 
-Given /^I set verbose option to '(.*)'$/ do |bool|
+Then /^That record's "([^"]*)" of the "([^"]*)" should be "([^"]*)"$/ do |sig, type, prop|
+  @record.send(type.to_sym).send(sig.to_sym).should == prop
+end
+
+Given /^I set verbose option to "(.*)"$/ do |bool|
   GitHub::Config::Options[:verbose] = bool
 end
