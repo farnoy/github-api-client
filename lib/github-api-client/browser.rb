ece1495d61f6e130a4b3f5fb78adff5077495e04
Jakub Okoński <kuba@okonski.org>
2011-05-07 10:20:16 +0200

    Ready up for v3 standard.

--- lib/github-api-client/browser.rb
+++ lib/github-api-client/browser.rb
@@ -4,26 +4,30 @@ module GitHub
     include Singleton
     
     # Returnes root uri for GitHub API
+    # @param [String] version GitHub api keyword, defaults to v2
+    # @option version "v1"
+    # @option version "v2"
+    # @option version "v3"
     # @return [String] Base GitHub API url for v2
-    def self.base_uri
-      "http://github.com/api/v2/yaml"
+    def self.base_uri(version = 'v2')
+      "http://github.com/api/#{version}/yaml"
     end
     
     # Runs HTTP GET request at given uri
     # @param [String] uri URI to be joined with base_uri and requested
     # @return [String] request result
-    def self.get(uri)
+    def self.get(uri, version = 'v2')
       uri = uri.gsub(" ","+")
-      puts "Requesting #{URI.parse(self.base_uri + uri)}" if GitHub::Config::Options[:verbose]
+      puts "Requesting #{URI.parse(self.base_uri(version) + uri)}" if GitHub::Config::Options[:verbose]
       Net::HTTP.get URI.parse(self.base_uri + uri)
     end
     
     # Runs HTTP POST requests with options such as GitHub::User.auth_info
     # @param [String] uri URI to be joined with base_uri and requested
     # @return [String] request result
-    def self.post(uri, options = {})
+    def self.post(uri, options = {}, version = 'v2')
       uri = uri.gsub(" ","+")
-      puts "Requesting #{URI.parse(self.base_uri + uri)} with options: #{options}" if GitHub::Config::Options[:verbose]
+      puts "Requesting #{URI.parse(self.base_uri(version) + uri)} with options: #{options}" if GitHub::Config::Options[:verbose]
       Net::HTTP.post_form URI.parse(self.base_uri + uri), options
     end
   end
