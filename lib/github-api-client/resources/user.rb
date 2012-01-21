aa258593283dda97715acd1a8c36ac0a6442c3ec
Jakub Okoński <jakub@okonski.org>
2012-01-21 19:21:20 +0100

    remove has_repos attribute from User

--- lib/github-api-client/resources/user.rb
+++ lib/github-api-client/resources/user.rb
@@ -1,7 +1,7 @@
 module GitHub
   module Resources
     class User
-      @@attributes = {login: :string, name: :string, has_repos: :boolean, location: :string, bio: :string, email: :string, hireable: :boolean, blog: :string}
+      @@attributes = {login: :string, name: :string, location: :string, bio: :string, email: :string, hireable: :boolean, blog: :string}
       @@pushables = [:name, :location, :bio, :email, :hireable, :blog]
       @@associations = {repositories: [nil, -> { has_many :repositories, :class_name => 'GitHub::Models::Repository'}]}
       include Resource
