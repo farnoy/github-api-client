9d7b260986f51799eea9b18078f4ce6646bec10b
Jakub Okoński <jakub@okonski.org>
2012-01-21 19:10:18 +0100

    provide a stub for user fetcher's #association_repositories method

--- lib/github-api-client/fetchers/user.rb
+++ lib/github-api-client/fetchers/user.rb
@@ -11,6 +11,15 @@ module GitHub
           user.attributes = user.class.valid_attributes(attributes)
         end
       end
+
+      def self.association_repositories(user)
+        attributes = {}
+        Browser.start do |http|
+          request = Net::HTTP::Get.new "/users/#{user.login}/repos"
+          attributes = JSON.parse(http.request(request).body, symbolize_names: true)
+        end
+        # further iterate and create Resources::Repository
+      end
     end
   end
 end
