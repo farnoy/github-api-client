d73a79702936063de40b1ebc86dc77902aaef2fc
Jakub Okoński <jakub@okonski.org>
2012-01-21 20:11:06 +0100

    add :company to pushables for User

--- lib/github-api-client/resources/user.rb
+++ lib/github-api-client/resources/user.rb
@@ -2,7 +2,7 @@ module GitHub
   module Resources
     class User
       @@attributes = {login: :string, name: :string, location: :string, bio: :string, email: :string, hireable: :boolean, blog: :string}
-      @@pushables = [:name, :location, :bio, :email, :hireable, :blog]
+      @@pushables = [:name, :location, :bio, :email, :hireable, :blog, :company]
       @@associations = {repositories: [nil, -> { has_many :repositories, :class_name => 'GitHub::Models::Repository'}]}
       include Resource
     end
