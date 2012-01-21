fe0a7dd5be4756c431607837f0d39d3d9ab7ac02
Jakub Okoński <jakub@okonski.org>
2012-01-21 18:59:09 +0100

    provide class for association User -> Repositories
    * replace constant object with string containing full path to Repo object

--- lib/github-api-client/resources/user.rb
+++ lib/github-api-client/resources/user.rb
@@ -3,7 +3,7 @@ module GitHub
     class User
       @@attributes = {login: :string, name: :string, has_repos: :boolean, location: :string, bio: :string, email: :string, hireable: :boolean, blog: :string}
       @@pushables = [:name, :location, :bio, :email, :hireable, :blog]
-      @@associations = {repositories: [nil, -> { has_many :repositories, :class_name => Repo}]}
+      @@associations = {repositories: [nil, -> { has_many :repositories, :class_name => 'GitHub::Models::Repository'}]}
       include Resource
     end
   end
