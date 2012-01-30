1d8156b7f55ac28d84dd36b0680182eb5904e78b
Jakub Okoński <jakub@okonski.org>
2012-01-30 21:56:58 +0100

    use mocha in examples

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -2,9 +2,12 @@ require 'minitest/spec'
 MiniTest::Unit.autorun
 require 'rspec/expectations'
 require 'rspec/matchers'
+require 'mocha'
 
 describe "Testing framework" do
   it "executes" do
-    assert_equal 3, 3
+    user = Struct.new("User", :name).new('kuba')
+    user.stubs(:age).returns(3)
+    3.should eq(user.age)
   end
 end
