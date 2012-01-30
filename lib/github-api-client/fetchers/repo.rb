635c510d15c0c4ea3404cef5eb934e58a5e44528
Jakub Okoński <jakub@okonski.org>
2012-01-30 19:29:17 +0100

    repo fetcher supports #association_owner
    * associate owner for repo in Fetchers::Repository#get
    * fire up Fetchers::User.get(repo.owner) for Fetchers::Repository#association_owner

--- lib/github-api-client/fetchers/repo.rb
+++ lib/github-api-client/fetchers/repo.rb
@@ -5,8 +5,8 @@ module GitHub
         def get(permalink)
           attributes = {}
           name = permalink.split('/').last
-          owner = Models::User.find_by_login(permalink.split('/').first)
-          model = owner.repositories.find_by_name(name) if owner
+          owner = Models::User.find_or_create_by_login(permalink.split('/').first)
+          model = owner.repositories.find_by_name(name)
           model ||= nil
           should_refresh = (if model then Config::Options[:strategy].should_refresh?(model); else false; end) # refactor
           if not model or should_refresh
@@ -14,12 +14,17 @@ module GitHub
               request = Net::HTTP::Get.new "/repos/#{permalink}"
               attributes = Fetchers.parse(http.request(request).body)
               model = Models::Repository.find_or_create_by_permalink(permalink)
+              model.owner = owner
               model.update_attributes(Resources::Repository.valid_attributes(attributes))
             end
           end
           repo = Resources::Repository.new.tap do |repo|
             repo.attributes = Resources::Repository.valid_attributes(model.attributes.symbolize_keys)
           end
+        end
+
+        def association_owner(repo)
+          User.get(repo.model.owner.login) # requires #get function to associate owner *always*
         end
       end
     end
